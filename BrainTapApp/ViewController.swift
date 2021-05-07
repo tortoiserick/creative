baebe7c1943dd9fea7bce065df953ecaaae1dc18
rhe_aon <rick.he@inpoint.com>
2021-05-07 09:14:59 -0400

    initial checkin

---
+++ BrainTapApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BrainTapApp
+//
+//  Created by Rick He on 5/10/20.
+//  Copyright © 2020 PineTech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
